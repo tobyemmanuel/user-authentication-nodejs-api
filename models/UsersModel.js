cb8f02004a09f89dcf6288b0887f561e5b4bc87f
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-08-08 05:16:52 +0100

    Created user model

---
+++ models/UsersModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose'); //import dependencies
+
+const UsersSchema = mongoose.Schema({ //create schema for the database table
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true
+    },
+    userType: {
+        type: String, 
+        enum: ["admin", "user", "staff", "manager"], 
+        default: "user"
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Users', UsersSchema);
